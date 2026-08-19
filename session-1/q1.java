92b9c24f4ba6bb0ef963f0614522a244aaab4011
Student-DBakyaSujitha <bakyasujitha5208@gmail.com>
2026-08-19 09:17:54 +0530

    Solved basic questions

---
+++ session-1/q1.java
@@ -0,0 +1,12 @@
+//Check Even or Odd
+public class q1 {
+    public static void main(String[] args) {
+        int number = 4;
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
---
+++ session-1/q6.java
@@ -0,0 +1,12 @@
+//Check Divisibility by 5
+public class q6 {
+    public static void main(String[] args) {
+        int number = 15;
+
+        if (number % 5 == 0) {
+            System.out.println("The number is divisible by 5.");
+        } else {
+            System.out.println("The number is not divisible by 5.");
+        }
+    }
+}
