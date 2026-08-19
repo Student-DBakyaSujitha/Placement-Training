ec8bd241dcbf64dca144be05dc0c5a042e913e15
Bakya Sujitha D <dbakyasujitha@gmail.com>
2026-08-19 09:06:40 +0530

    Add files via upload
    basic questions

---
+++ q2.java
@@ -0,0 +1,14 @@
+//Check Positive, Negative or Zero
+public class q2 {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
